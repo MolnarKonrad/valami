9b1a798376468a9f0ff93407a13a1674019587e5
Konrad <molnar.konrad@gmail.com>
2024-05-25 10:05:28 +0200

    metodus letrehozása

--- src/lokalisrepo/LokalisRepo.java
+++ src/lokalisrepo/LokalisRepo.java
@@ -3,6 +3,11 @@ package lokalisrepo;
 public class LokalisRepo {
 
     public static void main(String[] args) {
+        OsszegzesTetele();
+    }
+
+    private static void OsszegzesTetele() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
 }
